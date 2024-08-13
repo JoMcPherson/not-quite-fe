abd1ddf161a7be169c78206db716f6e9acd16d98
Juan.Pinol <juan.pinol@cvent.com>
2024-08-13 13:26:11 -0400

    got rid of console log in app and changed a tag to div to stop error

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
           "http://localhost:8080/events"
         );
         setEvents(response.data);
-        console.log(events);
+        // console.log(events);
       } catch (err) {
         setError("Failed to fetch events");
       } finally {
