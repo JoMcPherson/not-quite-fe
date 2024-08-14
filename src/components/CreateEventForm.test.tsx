4087658dfca6b1990880bce08bfef969f1cbdedc
McPherson <joanna.mcpherson@cvent.com>
2024-08-14 16:12:53 -0400

    test suites

--- src/components/CreateEventForm.test.tsx
+++ src/components/CreateEventForm.test.tsx
@@ -9,9 +9,18 @@ const sports = ["soccer", "basketball", "volleyball"];
 describe("CreateEventForm", () => {
   it("renders a form with input fields", async () => {
     render(<CreateEventForm user={user} sports={sports} />);
-    const olympicElement = await screen.findByText("Olympic Sport");
-    expect(olympicElement).toBeInTheDocument();
     const createEvent = await screen.findByText("Create New Event");
     expect(createEvent).toBeInTheDocument();
+    const olympicElement = await screen.findByText("Olympic Sport");
+    expect(olympicElement).toBeInTheDocument();
+  });
+  it("renders a dropdown of sports", async () => {
+    render(<CreateEventForm user={user} sports={sports} />);
+    const sportDropdown = await screen.findByLabelText("Olympic Sport");
+    expect(sportDropdown).toBeInTheDocument();
+    expect(sportDropdown).toHaveValue("");
+    expect(sportDropdown).toHaveTextContent("Select a sport");
+    sportDropdown.click();
+    expect(sportDropdown).toHaveTextContent("soccer");
   });
 });
--- src/components/EditEventForm.tsx
+++ src/components/EditEventForm.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
-import "../App.css";
 import { Event } from "../interfaces/Event";
 
 interface EditEventFormProps {
