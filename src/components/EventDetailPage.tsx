6fafc8b8b4353200b0ff6966f73b19583d62ca87
tbyrd2237 <tbyrd2237@gmail.com>
2024-08-20 19:28:57 -0400

    Fixed the code

--- src/components/EventDetailPage.tsx
+++ src/components/EventDetailPage.tsx
@@ -20,48 +20,59 @@ interface EventDetailPageProps {
 
 const EventDetailPage: React.FC<EventDetailPageProps> = ({ user, events }) => {
   const { eventId } = useParams<{ eventId: string }>();
-  const [selectedEvent, setSelectedEvent] = useState<any>(null); 
+  const selectedEvent = events.find((event) => event.id === parseInt(eventId!));
+  const [currentEvent, setCurrentEvent] = useState<any>(null); 
+  const eventCreator = selectedEvent?.cognitoUserId;
+
   const [isAttending, setIsAttending] = useState(false);
   const [loggedInUser, setLoggedInUser] = useState<string | null>(null);
+
   const [mapCenter, setMapCenter] = useState<google.maps.LatLngLiteral>({ lat: 37.7749, lng: -122.4194 }); // Default to San Francisco
   const [markerPosition, setMarkerPosition] = useState<google.maps.LatLngLiteral>({ lat: 37.7749, lng: -122.4194 }); // Default to San Francisco
   const [zoom] = useState(12);
 
-  const eventCreator = selectedEvent?.cognitoUserId;
   const apiKey = import.meta.env.VITE_API_KEY;
 
-  useEffect(() => {
-    const fetchEventDetails = async () => {
-      if (eventId) {
-        const event = events.find((e) => e.id === parseInt(eventId));
-        setSelectedEvent(event || null);
-      }
-    };
+    useEffect(() => {
+      const fetchEventData = async () => {
+        if (eventId) {
+          try {
+            const event = events.find((e) => e.id === parseInt(eventId));
+            setCurrentEvent(event || null);
+  
+            if (event) {
+              const address = `${event.street}, ${event.city}, ${event.state} ${event.zip}`;
+              try {
+                const response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json`, {
+                  params: {
+                    address,
+                    key: apiKey
+                  }
+                });
+                if (response.data.results.length > 0) {
+                  const location = response.data.results[0].geometry.location;
+                  setMapCenter({ lat: location.lat, lng: location.lng });
+                  setMarkerPosition({ lat: location.lat, lng: location.lng });
+                } else {
+                  console.error("No results found for address:", address);
+                }
+              } catch (error) {
+                console.error("Error fetching geocode:", error);
+              }
+  
+              const session = await fetchAuthSession();
+              const cognitoUserId = session?.tokens?.idToken?.payload?.sub;
+              setLoggedInUser(cognitoUserId || null);
 
-    fetchEventDetails();
-  }, [eventId, events]);
-
-  useEffect(() => {
-    const fetchTokenAndCheckAttendance = async () => {
-      try {
-        const session = await fetchAuthSession();
-        const cognitoUserId = session?.tokens?.idToken?.payload?.sub;
-        setLoggedInUser(cognitoUserId || null);
-
-        if (eventId && cognitoUserId) {
-          const isUserAttending = await checkAttendance(eventId, cognitoUserId);
-          setIsAttending(isUserAttending);
+            }
+          } catch (error) {
+            console.error("Error fetching event details or checking attendance:", error);
+          }
         }
-      } catch (error) {
-        console.error(
-          "Error fetching auth session or checking attendance:",
-          error
-        );
-      }
-    };
-
-    fetchTokenAndCheckAttendance();
-  }, [eventId]);
+      };
+  
+      fetchEventData();
+    }, [eventId, events, apiKey]);
 
   const handleAttendEvent = async () => {
     try {
@@ -96,101 +107,103 @@ const EventDetailPage: React.FC<EventDetailPageProps> = ({ user, events }) => {
 
   const spotsLeft = selectedEvent.maxAttendees - 5;
 
-  return (
-    <div className="flex flex-col min-h-screen w-full max-w-7xl mx-auto">
+ return (
+  <div className="flex flex-col min-h-screen w-full max-w-7xl mx-auto">
       <div className="flex justify-center py-8 px-4">
         <div className="w-full">
           <h1 className="text-3xl font-bold text-center mb-4">
             {selectedEvent.title}
           </h1>
           <div className="flex justify-center pb-8">
-            <Map
+          <Map
               center={mapCenter}
               zoom={zoom}
               markerPosition={markerPosition}
-            />
-          </div>
-          <div className="flex flex-col md:flex-row items-center bg-white border border-gray-200 rounded-lg shadow-lg hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 w-4/5 mx-auto">
-            <img
-              className="object-cover w-full md:w-1/2 h-96 md:h-auto rounded-t-lg md:rounded-l-lg"
-              src={selectedEvent.image}
-              alt={selectedEvent.title}
-            />
-            <div className="flex flex-col justify-between p-6 leading-normal w-full md:w-1/2 text-center">
-              <h5 className="mb-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-white">
-                {`${selectedEvent.street}, ${selectedEvent.city}, ${selectedEvent.state} ${selectedEvent.zip}`}
-              </h5>
-              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                {spotsLeft} spots left
-              </p>
-              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                <b>Timing: </b>
-                {new Date(selectedEvent.date).toLocaleString()}
-              </p>
-              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                <b>Hosted By: </b>
-                {selectedEvent.cognitoUserId || "Unknown Host"}
-              </p>
-              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                <b>Description: </b>
-                {selectedEvent.description}
-              </p>
-              <div className="flex justify-center">
-                {loggedInUser !== eventCreator ? (
-                  <button
-                    onClick={
-                      isAttending ? handleWithdrawEvent : handleAttendEvent
-                    }
-                    className={`inline-flex items-center px-4 py-3 text-sm font-medium text-center text-white ${
-                      isAttending
-                        ? "bg-[#ff0000] hover:bg-[#ff4d4d] focus:ring-[#ff6666] dark:bg-[#e60000] dark:hover:bg-[#ff3333] dark:focus:ring-[#ff4d4d]"
-                        : "bg-blue-700 hover:bg-blue-800 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                    } rounded-lg focus:outline-none`}
+          />
+        </div>
+        <div className="flex flex-col md:flex-row items-center bg-white border border-gray-200 rounded-lg shadow-lg hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 w-4/5 mx-auto">
+          <img
+            className="object-cover w-full md:w-1/2 h-96 md:h-auto rounded-t-lg md:rounded-l-lg"
+            src={selectedEvent.image}
+            alt={selectedEvent.title}
+          />
+          <div className="flex flex-col justify-between p-6 leading-normal w-full md:w-1/2 text-center">
+            <h5 className="mb-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-white">
+              {`${selectedEvent.street}, ${selectedEvent.city}, ${selectedEvent.state} ${selectedEvent.zip}`}
+            </h5>
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+              {spotsLeft} spots left
+            </p>
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+              <b>Timing: </b>
+              {new Date(selectedEvent.date).toLocaleString()}
+            </p>
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+              <b>Hosted By: </b>
+              {selectedEvent.cognitoUserId || "Unknown Host"}
+            </p>
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+              <b>Description: </b>
+              {selectedEvent.description}
+            </p>
+            <div className="flex justify-center">
+              {loggedInUser !== eventCreator ? (
+                <button
+                  onClick={
+                    isAttending ? handleWithdrawEvent : handleAttendEvent
+                  }
+                  data-testid="signUp"
+                  className={`inline-flex items-center px-4 py-3 text-sm font-medium text-center text-white ${
+                    isAttending
+                      ? "bg-[#ff0000] hover:bg-[#ff4d4d] focus:ring-[#ff6666] dark:bg-[#e60000] dark:hover:bg-[#ff3333] dark:focus:ring-[#ff4d4d]"
+                      : "bg-blue-700 hover:bg-blue-800 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  } rounded-lg focus:outline-none`}
+                >
+                  {isAttending ? "Withdraw" : "Sign up"}
+                  <svg
+                    className={`rtl:rotate-180 w-4 h-4 ms-2 ${
+                      isAttending ? "" : "transform rotate-0"
+                    }`}
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 10"
                   >
-                    {isAttending ? "Withdraw" : "Sign up"}
-                    <svg
-                      className={`rtl:rotate-180 w-4 h-4 ms-2 ${
-                        isAttending ? "" : "transform rotate-0"
-                      }`}
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 14 10"
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M1 5h12m0 0L9 1m4 4L9 9"
+                    />
+                  </svg>
+                </button>
+              ) : (
+                loggedInUser === eventCreator && (
+                  <div className="flex justify-center mt-4 space-x-2">
+                    <a
+                      href={`/edit/${eventId}`}
+                      className="inline-flex items-center px-4 py-3 text-sm font-medium text-center text-white bg-pink-500 rounded-lg hover:bg-pink-600 focus:ring-4 focus:outline-none focus:ring-pink-300 dark:bg-pink-600 dark:hover:bg-pink-700 dark:focus:ring-pink-800"
                     >
-                      <path
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M1 5h12m0 0L9 1m4 4L9 9"
-                      />
-                    </svg>
-                  </button>
-                ) : (
-                  loggedInUser === eventCreator && (
-                    <div className="flex justify-center mt-4 space-x-2">
-                      <a
-                        href={`/edit/${eventId}`}
-                        className="inline-flex items-center px-4 py-3 text-sm font-medium text-center text-white bg-pink-500 rounded-lg hover:bg-pink-600 focus:ring-4 focus:outline-none focus:ring-pink-300 dark:bg-pink-600 dark:hover:bg-pink-700 dark:focus:ring-pink-800"
-                      >
-                        Edit Event
-                      </a>
-                      <button
-                        onClick={() => handleDeleteEvent(selectedEvent.id)}
-                        className="inline-flex items-center px-4 py-3 text-sm font-medium text-center text-white bg-pink-500 rounded-lg hover:bg-pink-600 focus:ring-4 focus:outline-none focus:ring-pink-300 dark:bg-pink-600 dark:hover:bg-pink-700 dark:focus:ring-pink-800"
-                      >
-                        Delete Event
-                      </button>
-                    </div>
-                  )
-                )}
-              </div>
+                      Edit Event
+                    </a>
+                    <button
+                      onClick={() => handleDeleteEvent(selectedEvent.id)}
+                      className="inline-flex items-center px-4 py-3 text-sm font-medium text-center text-white bg-pink-500 rounded-lg hover:bg-pink-600 focus:ring-4 focus:outline-none focus:ring-pink-300 dark:bg-pink-600 dark:hover:bg-pink-700 dark:focus:ring-pink-800"
+                      data-testid="delete"
+                    >
+                      Delete Event
+                    </button>
+                  </div>
+                )
+              )}
             </div>
           </div>
         </div>
-      </div>
-    </div>
-  );
+        </div>
+          </div>
+        </div>
+);
 };
 
 export default EventDetailPage;
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,47 +1,35 @@
 import React, { useEffect, useRef, useState } from 'react';
 import "../styles/map.css";
 
-//Define a interface for the props
 export interface MapProps {
   center: google.maps.LatLngLiteral; 
   zoom: number; 
   markerPosition?: google.maps.LatLngLiteral; 
 }
 
-//Define the component (Input: Center, Zoom and MarkerPosition)
 const Map: React.FC<MapProps> = ({ center, zoom, markerPosition })  => {
-  //Creates a ref to hold DOM element
   const mapRef = useRef(null);
   const [map, setMap] = useState<google.maps.Map | null>(null); // Track the map instance
 
-  //Gets the API variable
   const apiKey = import.meta.env.VITE_API_KEY as string;
 
-  //Load the Google Maps API
   const loadGoogleMapsApiAsync = (apiKey: string) => {
-    //Return promise
     return new Promise<void>((resolve, reject) => {
-      //If the script is already added
       if (document.querySelector(`script[src*="maps.googleapis.com"]`)) {
-        //resolve the promise
         resolve();
         return;
       }
 
-      //Create a new script element
       const script = document.createElement('script');
-      //Sets the script to inclde the API key
       script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places&v=quarterly&loading=async`;
       script.async = true;
       script.defer = true;
       script.onload = () => resolve();
       script.onerror = (error) => reject(error);
-      //Adds the script to the document
       document.head.appendChild(script);
     });
   };
 
-  //Function to inalize the map
   const initializeMap = () => {
     if (mapRef.current && !map) {
       const newMap = new google.maps.Map(mapRef.current, {
@@ -50,7 +38,6 @@ const Map: React.FC<MapProps> = ({ center, zoom, markerPosition })  => {
       });
       setMap(newMap);
 
-      // Add marker if position is provided
       if (markerPosition) {
         new google.maps.marker.AdvancedMarkerElement({
           map: newMap,
@@ -60,7 +47,6 @@ const Map: React.FC<MapProps> = ({ center, zoom, markerPosition })  => {
     }
   };
 
-  //Load and initialize the map
   const loadGoogleMapsApi = async () => {
     try {
       await loadGoogleMapsApiAsync(apiKey);
