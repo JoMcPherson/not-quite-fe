7fdc37c9f9f4cadbdaaa47a1bf2d3e65be6433f3
tbyrd2237 <tbyrd2237@gmail.com>
2024-08-20 16:15:54 -0400

    Moved the styles to a style sheet

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,76 +1,6 @@
-// import React, { useEffect, useRef } from 'react';
-
-// //Declare the interface of the props
-// interface MapProps {
-//   center: google.maps.LatLngLiteral;
-//   zoom: number;
-//   markerPosition?: google.maps.LatLngLiteral;
-// }
-
-// //Create a variable to hold the style of the map container (can this be moved to a style sheet?)
-// const mapContainerStyle = {
-//   height: "300px",
-//   width: "90%"
-// };
-
-// //Pass in the center, zoom, and markerPosition props
-// const Map: React.FC<MapProps> = ({ center, zoom, markerPosition }) => {
-//   const mapRef = useRef<HTMLDivElement>(null);
-
-//   //Use the useEffect hook to load the Google Maps API and initialize the map
-//   useEffect(() => {
-//     const loadGoogleMapsApi = () => {
-//       if (!window.google) {
-//         const script = document.createElement('script');
-//         script.src = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_API_KEY}&libraries=places`;
-//         script.async = true;
-//         script.onload = () => {
-//           initializeMap();  // Initialize the map only after the script has loaded
-//         };
-//         script.onerror = (error) => {
-//           console.error("Error loading Google Maps API:", error);
-//         };
-//         document.head.appendChild(script);
-//       } else {
-//         initializeMap();
-//       }
-//     };
-
-//     //Initialize the map
-//     const initializeMap = () => {
-//       if (mapRef.current) {
-//         const map = new google.maps.Map(mapRef.current, {
-//           center,
-//           zoom,
-//         });
-
-//         if (markerPosition) {
-//           new google.maps.Marker({
-//             map,
-//             position: markerPosition,
-//           });
-//         }
-//       } else {
-//         console.error("Google Maps API not loaded.");
-//       }
-//     };
-
-//     loadGoogleMapsApi();
-//   }, [center, zoom, markerPosition]);
-
-//   return (
-//   <div ref={mapRef} style={mapContainerStyle} />
-//   );
-// };
-
-// export default Map;
-
 import React, { useEffect, useRef } from 'react';
+import "../styles/map.css";
 
-const mapContainerStyle = {
-  height: "400px",
-  width: "100%"
-};
 
 const Map = ({ center, zoom, markerPosition }) => {
   const mapRef = useRef(null);
@@ -97,7 +27,6 @@ const Map = ({ center, zoom, markerPosition }) => {
 
   const initializeMap = () => {
     if (typeof google === 'undefined') {
-      console.error("Google Maps API not loaded.");
       return;
     }
     if (mapRef.current) {
@@ -130,7 +59,7 @@ const Map = ({ center, zoom, markerPosition }) => {
   }, [center, zoom, markerPosition]);
 
   return (
-    <div ref={mapRef} style={mapContainerStyle} />
+    <div ref={mapRef} className="mapContainer" />
   );
 };
 
