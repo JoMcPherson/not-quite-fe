101db229519506494cdea2a9246d64e9b5e9471d
McPherson <joanna.mcpherson@cvent.com>
2024-08-22 13:32:32 -0400

    buildspec

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"], // Example for splitting vendor libraries
+          // Add more chunks if needed
+        },
+      },
+    },
+    chunkSizeWarningLimit: 500, // Adjust the chunk size warning limit as needed
+  },
+});
